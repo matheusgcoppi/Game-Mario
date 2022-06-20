bd9850025b12a9452ee149cd6179bde311342718
matheusgcoppi <matheuscoppi22@gmail.com>
2022-06-20 17:45:15 -0300

    Update

--- js/script.js
+++ js/script.js
@@ -1,11 +1,26 @@
 const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
 
 const jump = function () {
     mario.classList.add('jump');
+    
     setTimeout(() => { 
         mario.classList.remove('jump');
     }, 500); // wait 500ms to remove class jump, because the time of jump is 500ms
 }
+    const loop = setInterval(() => {
+
+        const pipePositionLeft = pipe.offsetLeft;
+        
+        if(pipePositionLeft <= 120) {
+            pipe.style.animation = 'none';
+            pipe.style.left = `${pipePositionLeft}px`;
+            
+        }
+
+    }, 10);
+    
+
 
 document.addEventListener('keypress', jump);
 
