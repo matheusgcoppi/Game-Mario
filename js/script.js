7d1e4d5c979030a19965e0ce2dd700979b4268bc
matheusgcoppi <matheuscoppi22@gmail.com>
2022-06-21 13:52:43 -0300

    Finished

--- js/script.js
+++ js/script.js
@@ -11,11 +11,24 @@ const jump = function () {
     const loop = setInterval(() => {
 
         const pipePositionLeft = pipe.offsetLeft;
+        const marioPositionBottom = Number(window.getComputedStyle(mario).bottom.replace('px', ''));
+        // Here I put Number to become a Number instead of a string, without replace it's not possible
+        // And window.. style(mario) is to get a bottom of a style
+    
+    
         
-        if(pipePositionLeft <= 120) {
+        if(pipePositionLeft <= 80 && pipePositionLeft > 0 && marioPositionBottom < 55) {
             pipe.style.animation = 'none';
             pipe.style.left = `${pipePositionLeft}px`;
-            
+
+            mario.style.animation = 'none';
+            mario.style.bottom = `${marioPositionBottom}px`;
+
+            mario.src = './assets/game-over.png';
+            mario.style.width = '50px';
+            mario.style.marginLeft = '60px';
+
+            clearInterval(loop); //to stop the loop
         }
 
     }, 10);
